10451e9d9d5323d5bd959aab8ad4a8193de9b19b
coyiutoc <coyiutoc@gmail.com>
2017-04-09 14:29:07 -0700

    added resume

--- javascript.js
+++ javascript.js
@@ -1,13 +1,11 @@
-$( document ).ready(function() {
-  $("#laptop_button").click(function() {
-      $('html,body').animate({
-          scrollTop: $(".block1").offset().top},
-          'slow');
-  });
+$("#laptop_button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".block1").offset().top},
+        'slow');
+});
 
-  $("#brain_button").click(function() {
-      $('html,body').animate({
-          scrollTop: $(".block2").offset().top},
-          'slow');
-  });
-}
+$("#brain_button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".block2").offset().top},
+        'slow');
+});
